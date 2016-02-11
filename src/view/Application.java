588603fc1a4da02e1bd4a24ffbf0abeef040f829
benny123 <benchen23@hotmail.com>
2016-02-11 16:42:04 -0500

    lit fichier

--- src/view/Application.java
+++ src/view/Application.java
@@ -1,12 +1,31 @@
 package view;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
 public class Application {
 
 	/**
 	 * Launch the application
 	 * @param args This parameter is ignored
 	 */
-	public static void main(String args[]) {
-		System.out.println("Audio Resample project!");
+	private static FileInputStream fichier_wav;
+	
+	public static void main(String args[]) throws IOException {
+		
+		 System.out.println(args[0]);
+		 try {
+			fichier_wav = new FileInputStream(args[0]);
+			System.out.println(fichier_wav.read());
+			
+			//lire si c'est un fichier
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
 	}
 }
---
+++ src/view/WAVAudioFilter.java
@@ -0,0 +1,12 @@
+package view;
+
+
+public class WAVAudioFilter implements AudioFilter{
+
+	@Override
+	public void process() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
